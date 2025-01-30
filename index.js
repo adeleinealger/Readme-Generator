abf98883dddb038231b3077e7245c75a5e625b04
Adeleine Alger <adeleinealger@gmail.com>
2025-01-30 16:16:30 -0700

    Create questions array and use it to prompt user; Create function to generate README from user input

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
-// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from './utils/generateMarkdown.js';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of this application?', 'Please input the description for your application.', 'Please input installation instructions.',
+    'Please input usage information.', 'Please input contribution guidlines.', 'Please input test instructions.', 'Select a licence', 'Enter your GitHub username',
+    'Enter your email address.'
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: questions[0],
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: questions[1],
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: questions[2],
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: questions[3],
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: questions[4],
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: questions[5],
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: [
+            'Apache License 2.0', 'GNU General Public License 3.0', 'MIT License', 'BSD 2-Clause License', 'BSD 3-Clause License', 'Creative Commons Zero 1.0 Universal',
+            'Eclipse Public License', 'Mozilla Public License 2.0'
+        ],
+        message: questions[6],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: questions[7],
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: questions[8],
+    },
+  ])
+  .then((answers) => {
+    const readMeContent = generateMarkdown(answers);
+    fs.writeFile('testREADME.md', readMeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,43 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contribution}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+
+  If you have questions, please feel free to contact me at ${data.email}, or here on GitHub at ${data.github}
+
+  ## License
+
+  This repository is licensed under the ${data.license} license. See the license badge for more information.
+  `;
 }
 
 export default generateMarkdown;

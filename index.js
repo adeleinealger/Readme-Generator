334d0d380bd88fe2f6a917a29de52561bcae9c62
Adeleine Alger <adeleinealger@gmail.com>
2025-01-30 16:49:07 -0700

    Complete init function

--- index.js
+++ index.js
@@ -8,69 +8,70 @@ const questions = [
     'Enter your email address.'
 ];
 
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: questions[0],
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: questions[1],
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: questions[2],
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: questions[3],
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: questions[4],
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: questions[5],
-    },
-    {
-        type: 'list',
-        name: 'license',
-        choices: [
-            'Apache License 2.0', 'GNU General Public License 3.0', 'MIT License', 'BSD 2-Clause License', 'BSD 3-Clause License', 'Creative Commons Zero 1.0 Universal',
-            'Eclipse Public License', 'Mozilla Public License 2.0'
-        ],
-        message: questions[6],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: questions[7],
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: questions[8],
-    },
-  ])
-  .then((answers) => {
-    const readMeContent = generateMarkdown(answers);
-    fs.writeFile('README.md', readMeContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created README.md!')
-    );
-  });
-
-// TODO: Create a function to initialize app
-function init() {
-
+function inquire() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[3],
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: questions[4],
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[5],
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: [
+                'Apache License 2.0', 'GNU General Public License 3.0', 'MIT License', 'BSD 2-Clause License', 'BSD 3-Clause License', 'Creative Commons Zero 1.0 Universal',
+                'Eclipse Public License', 'Mozilla Public License 2.0'
+            ],
+            message: questions[6],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: questions[7],
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[8],
+        },
+    ])
+    .then((answers) => {
+        const readMeContent = generateMarkdown(answers);
+        fs.writeFile('README.md', readMeContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
 }
 
-// Function call to initialize app
+function init() {
+  console.log('Welcome to README generator!');
+  inquire();
+}
+
 init();
